e13f263b92e113aeb3afb9e4573316eac39cfde0
Tim Douglas <tim.douglas@phocassoftware.com>
2020-09-30 18:09:01 +1000

    More sounds

--- Reported2_Sounds.lua
+++ Reported2_Sounds.lua
@@ -6,4 +6,18 @@ function PlaySwearDetectedSound()
   end
 end
 
+function PlayReportMadeSound()
+  if not REPORTED2_PREFS[REPORTED2_PREFS_MUTE_SOUNDS] then
+    PlaySoundFile([[Interface\Addons\Reported2\report-made.mp3]])
+  end
+end
+
+function PlaySkipSound()
+  if not REPORTED2_PREFS[REPORTED2_PREFS_MUTE_SOUNDS] then
+    PlaySoundFile([[Interface\Addons\Reported2\skip.mp3]])
+  end
+end
+
 sounds.PlaySwearDetectedSound = PlaySwearDetectedSound
+sounds.PlayReportMadeSound = PlayReportMadeSound
+sounds.PlaySkipSound = PlaySkipSound
