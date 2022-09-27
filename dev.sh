26f4634b967d7cb0848878b3edce21e842b5ad6a
Tim Douglas <neurotech@gmail.com>
2022-09-27 13:09:41 +1000

    Updated TOCs

--- dev.sh
+++ dev.sh
@@ -10,11 +10,16 @@ cat Reported2.toctemplate.tmp > Reported2-Classic.toc
 sed -i "s/INTERFACE_VERSION/$(cat ./versions/retail)/g" Reported2-Retail.toc
 sed -i "s/INTERFACE_VERSION/$(cat ./versions/classic)/g" Reported2-Classic.toc
 
-cp *.lua *.mp3 *.tga *.ttf /d/games/World\ of\ Warcraft/_retail_/Interface/AddOns/Reported2/
-cp Reported2-Retail.toc /d/games/World\ of\ Warcraft/_retail_/Interface/AddOns/Reported2/Reported2.toc
+mkdir -p /h/games/World\ of\ Warcraft/_retail_/Interface/AddOns/Reported2/
+mkdir -p /h/games/World\ of\ Warcraft/_classic_/Interface/AddOns/Reported2/
 
-cp *.lua *.mp3 *.tga *.ttf /d/games/World\ of\ Warcraft/_classic_/Interface/AddOns/Reported2/
-cp Reported2-Classic.toc /d/games/World\ of\ Warcraft/_classic_/Interface/AddOns/Reported2/Reported2.toc
+cp *.lua *.mp3 *.tga *.ttf /h/games/World\ of\ Warcraft/_retail_/Interface/AddOns/Reported2/
+cp Reported2-Retail.toc /h/games/World\ of\ Warcraft/_retail_/Interface/AddOns/Reported2/
+
+cp *.lua *.mp3 *.tga *.ttf /h/games/World\ of\ Warcraft/_classic_/Interface/AddOns/Reported2/
+cp Reported2-Classic.toc /h/games/World\ of\ Warcraft/_classic_/Interface/AddOns/Reported2/
+cp Reported2-Classic.toc /h/games/World\ of\ Warcraft/_classic_/Interface/AddOns/Reported2/Reported2-BCC.toc
+cp Reported2-Classic.toc /h/games/World\ of\ Warcraft/_classic_/Interface/AddOns/Reported2/Reported2-WOTLKC.toc
 
 rm Reported2.toctemplate.tmp
 rm Reported2-Retail.toc
